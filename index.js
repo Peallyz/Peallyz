eb546cb68561082a8b52db464ed4a71d583585d1
Peally <pieclandre@gmail.com>
2023-08-09 09:24:49 +0200

    feat : update readme

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+
+function readReadmeFile(filePath) {
+  try {
+    const content = fs.readFileSync(filePath, "utf-8");
+    return content;
+  } catch (error) {
+    console.error("Error reading the file:", error);
+    return null;
+  }
+}
+
+const readmeFilePath = "README.md";
+const readmeContent = readReadmeFile(readmeFilePath);
+
+if (!readmeContent) {
+  console.error("Readme content could not be read.");
+  process.exit(1);
+}
+
+function updateExperienceDays(content) {
+  const regex = /💻 (\d+) days of experiences and practices\./;
+  const match = content.match(regex);
+
+  if (match && match[1]) {
+    const currentDays = parseInt(match[1]);
+    const newDays = currentDays + 1;
+    const updatedContent = content.replace(
+      regex,
+      `💻 ${newDays} days of experiences and practices.`
+    );
+    return updatedContent;
+  } else {
+    console.error("Experience days not found in the content.");
+    return null;
+  }
+}
+
+const updatedReadmeContent = updateExperienceDays(readmeContent);
+
+if (!updatedReadmeContent) {
+  console.error("Updating experience days failed.");
+  process.exit(1);
+}
+
+function writeReadmeFile(filePath, content) {
+  try {
+    fs.writeFileSync(filePath, content, "utf-8");
+    console.log("Readme file updated successfully.");
+  } catch (error) {
+    console.error("Error writing the file:", error);
+  }
+}
+
+writeReadmeFile(readmeFilePath, updatedReadmeContent);
